8c12ae503a3b120c7932ae8c9ca5a161f06ffa12
Mr-315 <someonempty@gmail.com>
2022-06-28 18:16:21 +0300

    [верстка]бордеры в соотвествии с макетом + отправляет input по нажатию на enter

--- app.js
+++ app.js
@@ -3,13 +3,28 @@ const button = document.querySelector('.adding-button')
 const input = document.querySelector('.task-input')
 const list = document.querySelector('.items-list')
 
-button.addEventListener('click', () => {
 
+function createTask () {
     const div = document.createElement('div')
     div.classList.add('task')
     div.innerHTML = input.value
 
     list.appendChild(div)
     input.value = ''
+}
+
+
+
+button.addEventListener('click', () => {
+
+    createTask()
 }) 
 
+
+input.addEventListener('keydown', function(e) {
+    if (e.keyCode === 13) 
+      {
+      createTask()
+      }
+    })
+   
