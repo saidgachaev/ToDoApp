22b3ef6e9ee34e47dbafd8df9f3f75b3ac754781
Mr-315 <someonempty@gmail.com>
2022-06-29 21:08:10 +0300

    [вертска] убрать закругления у задач между первой и последней

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const button = document.querySelector('.adding-button')
 const input = document.querySelector('.task-input')
 const list = document.querySelector('.items-list')
+const task = document.querySelector('.task')
+
+
 
 
 function createTask () {
@@ -11,8 +14,11 @@ function createTask () {
     const div = document.createElement('div')
     div.classList.add('task')
     div.innerHTML = input.value
-
     list.appendChild(div)
+
+    const deleteButton = document.createElement('button')
+    deleteButton.classList.add('delete-button')
+    task.appendChild(deleteButton)
     input.value = ''
     }
     
@@ -31,4 +37,3 @@ input.addEventListener('keydown', function(e) {
       createTask()
       }
     })
-   
