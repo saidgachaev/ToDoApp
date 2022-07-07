5585ba1893684b90db6c8bd8b97fde25c0087554
someonempty <someonempty@gmail.com>
2022-07-08 01:38:49 +0300

    [фича] кнопка удаления

--- app.js
+++ app.js
@@ -2,31 +2,34 @@
 const button = document.querySelector('.adding-button')
 const input = document.querySelector('.task-input')
 const list = document.querySelector('.items-list')
-const task = document.querySelector('.task')
 
 
 
-
+  
 function createTask () {
 
     if(input.value != '')
     {
-    const div = document.createElement('div')
-    div.classList.add('task')
-    div.innerHTML = input.value
-    list.appendChild(div)
+    const createElement = document.createElement('div')
+    createElement.classList.add('task')
+    createElement.innerHTML = input.value
+    list.appendChild(createElement)
+    createElement.id = Math.random()
 
     const deleteButton = document.createElement('button')
     deleteButton.classList.add('delete-button')
-    task.appendChild(deleteButton)
-    input.value = ''
+    createElement.appendChild(deleteButton)
+    deleteButton.onclick = () => {
+        document.getElementById(createElement.id).remove()
     }
     
+    input.value = ''
+    }
 }
 
 
-button.addEventListener('click', () => {
 
+button.addEventListener('click', () => {
     createTask()
 }) 
 
