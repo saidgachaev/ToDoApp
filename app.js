252d76d706ed332c30219b7b3450bc4de9a532bb
someonempty <someonempty@gmail.com>
2022-08-09 19:14:47 +0300

    Добавляет небольшие улучшения кода

--- app.js
+++ app.js
@@ -1,42 +1,54 @@
 
-const button = document.querySelector('.adding-button')
-const input = document.querySelector('.task-input')
-const list = document.querySelector('.items-list')
+// Получаю элементы из HTML
 
+const button = document.querySelector('.adding-button'),
+      input = document.querySelector('.task-input'),
+      list = document.querySelector('.items-list');
 
+let newTask = '',
+        deleteButton = '';
 
-  
 function createTask () {
 
     if(input.value != '')
     {
-    const newTask = document.createElement('div')
-    newTask.classList.add('task')
-    newTask.innerHTML = input.value
-    list.appendChild(newTask)
-    newTask.id = Math.random()
+    
+    newTask = document.createElement('div');
+    list.appendChild(newTask);
+    newTask.classList.add('task');
+    newTask.id = Math.random();
 
-    const deleteButton = document.createElement('button')
-    deleteButton.classList.add('delete-button')
-    newTask.appendChild(deleteButton)
+    const checkbox = document.createElement('INPUT');
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.classList.add('checkbox-style');
+
+    let elementContainer = document.createElement('div');
+        elementContainer.appendChild(checkbox);
+        elementContainer.innerHTML += input.value;
+        newTask.appendChild(elementContainer);
+        
+
+    deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-button');
+    newTask.appendChild(deleteButton);
+
     deleteButton.onclick = () => {
-        document.getElementById(newTask.id).remove()
+        document.querySelector('.task').remove();
     }
     
-    input.value = ''
+    input.value = '';
     }
 }
 
 
-
 button.addEventListener('click', () => {
-    createTask()
+    createTask();
 }) 
 
 
 input.addEventListener('keydown', function(e) {
     if (e.keyCode === 13) 
       {
-      createTask()
+      createTask();
       }
     })
